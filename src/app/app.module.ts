8ee90eb133465231fe5a34788a801b14687a5eaf
wardtyler96 <101146280+wardtyler96@users.noreply.github.com>
2022-04-18 16:26:42 -0400

    ConsoleLog

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import{HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+  ],
+  imports: [
+    BrowserModule, FormsModule, HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+ _http:HttpClient;
+
+ 
+
+ signUpForm:any = [ ];
+ constructor(_httpRef:HttpClient) {
+  this._http = _httpRef;
+  
+  
+ }
+
+  getForm() {
+    this._http.get('#SignUp')
+  }
+
+  onSignUp(value:any) {
+   // console.log(this._http.post('https://localhost:44340/api/User', "{email: 'eventit@email', Password: 'test', name:'Gerome'}",{headers:new HttpHeaders({'Content-Type':'application/json'})}) );
+    console.log(value)
+
+  }
+  onLogin() {
+    
+  }
+ 
+
+  ngOnInit(): void {
+  }
+
+}
