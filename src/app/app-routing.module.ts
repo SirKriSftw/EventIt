1527e8b9b2bcd032653d5299966e3c4200ee7b56
wardtyler96 <101146280+wardtyler96@users.noreply.github.com>
2022-04-19 11:34:35 -0400

    RoutingPrblem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlansComponent } from './components/plans/plans.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [ 
+  {path: "plans", component: PlansComponent},
+  {path: "login", component:UsersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import{HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import { PlansComponent } from './components/plans/plans.component';
+import {RouterModule} from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -12,9 +20,19 @@ import { UsersComponent } from './components/users/users.component';
   declarations: [
     AppComponent,
     UsersComponent,
+    PlansComponent,
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, 
+    FormsModule,
+     HttpClientModule, 
+     RouterModule, 
+     MatCardModule, 
+     MatButtonModule, 
+     FlexLayoutModule, 
+     MatToolbarModule,
+     BrowserAnimationsModule,
+     AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
